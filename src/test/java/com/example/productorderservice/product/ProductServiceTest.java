f021b1ad6a6fa761522dff3339b55240ad586ccf
Gwancheol-Lee <rhkscjf9785@naver.com>
2023-02-10 23:59:57 +0900

    Create Product Class

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -25,7 +25,10 @@ class ProductServiceTest {
 	
 	private class ProductService {
 		public void addProduct(final AddProductRequest request) {
-			throw new UnsupportedOperationException("Unsupported addProduct");
+			final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+			
+			productPort.save(product);
+			// throw new UnsupportedOperationException("Unsupported addProduct");
 		}
 	}
 	// 레코드 클래스는 불변 데이터 객체를 쉽게 생성할 수 있도록 해줌. 생성자 선언, getter, setter, toString, equals, hashCode 등의 메소드 생략 가능 
@@ -36,29 +39,14 @@ class ProductServiceTest {
 			Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
 		}
 	}
-	// 기존 아래와 같이 생성자 선언 할 필요 없이 위 레코드 클래스로 코드를 깔끔하게 작성할 수 있다.
-	/*
-	private static class AddProductRequest {
-
-		private final String name;
-		private final int price;
-		private final DiscountPolicy discountPolicy;
-		
-		public AddProductRequest(final String name, final int price, final DiscountPolicy discountPolicy) {
-			this.name = name;
-			this.price = price;
-			this.discountPolicy = discountPolicy;
-			Assert.hasText(name, "상품명은 필수입니다.");
-			Assert.isTrue(price>0, "상품 가격은 0보다 커야 합니다.");
-			Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-		}
-		
-	}
-	*/
-	
-
 	
 	private enum DiscountPolicy {
 		NONE
+	}
+	
+	private class Product {
+		public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
+			
+		}
 	}
 }
