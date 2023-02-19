3d32b23ea9753057eb3af395c8c585a868954706
Gwancheol-Lee <rhkscjf9785@naver.com>
2023-02-19 21:53:45 +0900

    Test discounted price value

--- src/test/java/com/example/productorderservice/product/DiscountPolicyTest.java
+++ src/test/java/com/example/productorderservice/product/DiscountPolicyTest.java
@@ -16,12 +16,22 @@ class DiscountPolicyTest {
 	}
 	
 	@Test
-	void name() {
+	void fix_1000_discounted_price() {
 		final int price = 2000;
 		
 		// 1000원 고정 할인 정책 적용 시 price - 1000
 		final int discountedPrice = DiscountPolicy.FIX_1000_AMOUNT.applyDiscount(price);
 		
 		assertThat(discountedPrice).isEqualTo(1000);
+	}
+	
+	@Test
+	void over_discounted_price() {
+		final int price = 500;
+		
+		// 500원보다 더 할인했을시 0원인지 확인
+		final int discountedPrice = DiscountPolicy.FIX_1000_AMOUNT.applyDiscount(price);
+		
+		assertThat(discountedPrice).isEqualTo(0);
 	}
 }
